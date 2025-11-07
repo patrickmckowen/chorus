a99b28437eaa820126929dc6739b70f6f89c4e48
patrickmckowen <patrick.mckowen@gmail.com>
2025-11-07 15:07:52 -0800

    chore: dotenv-driven app.config with extra

---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+import Constants from 'expo-constants';
+
+type ExtraConfig = {
+	API_URL: string;
+};
+
+export const appConfig = {
+	apiUrl: (Constants?.expoConfig?.extra as ExtraConfig | undefined)?.API_URL ?? 'https://example.com',
+};
+
+
